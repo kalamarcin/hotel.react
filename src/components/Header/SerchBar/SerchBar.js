66454f4b7f3478740c00baee72bd9f7677421cc0
kalamarcin <kalamarcin1@gmail.com>
2022-09-29 10:10:27 +0200

    styles and search

--- src/components/Header/SerchBar/SerchBar.js
+++ src/components/Header/SerchBar/SerchBar.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SerchBar() {
+	const [term, setTerm] = useState('')
+	const search = () => {
+		console.log(term)
+	}
+	const onKeyHandler = e => {
+		if (e.key === 'Enter') {
+			search()
+		}
+	}
+
 	return (
 		<div className='d-flex'>
-			<input className='form-control' type='text' placeholder='Szukaj...' />
-			<button className='btn btn-primary'>Szukaj</button>
+			<input
+				value={term}
+				onChange={e => {
+					setTerm(e.target.value)
+				}}
+				onKeyDown={onKeyHandler}
+				className='form-control'
+				type='text'
+				placeholder='Szukaj...'
+			/>
+
+			<button onClick={search} className='ms-2 btn btn-primary'>
+				Szukaj
+			</button>
 		</div>
 	)
 }
--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -4,31 +4,34 @@ import img from '../../../assets/images/hotel-1749602_1920.jpg'
 
 function Hotel() {
 	return (
-		<div className={`row ${style.hotel}`}>
-			<div className='col-4'>
-				<img src={img} alt='' className='img-fluid' />
-			</div>
-			<div className='col-8 margin'>
+		<div className='card mb-4'>
+			<div className={`card-body pb-4 ${style.hotel}`}>
 				<div className='row'>
-					<div className='col'>
-						<p>Tytuł</p>
-						<p>miasto</p>
+					<div className='col-4'>
+						<img src={img} alt='' className='img-fluid img-thumbnail' />
 					</div>
-					<div className='col'>
-						<p>Ocena urzytkownikow</p>
-						<p>Opinie: 233</p>
+					<div className='col-8'>
+						<div className='row'>
+							<div className='col'>
+								<p className={style.title}>Pensjonat</p>
+								<span className='badge bg-light text-dark'>Gdańsk</span>
+							</div>
+							<div className='col text-end'>
+								<h5>Ocena: 8.3</h5>
+								<a href='./' className='btn btn-primary mt-2 px-5 '>
+									Pokaż
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className='col-12'>
+						<p className={style.description}>
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+							industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+							scrambled it to make a type specimen book.
+						</p>
 					</div>
 				</div>
-			</div>
-			<div className='col-12'>
-				<p>
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-					standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-					make a type specimen book. 
-				</p>
-				<a href='./' className='btn btn-primary'>
-					Pokaz
-				</a>
 			</div>
 		</div>
 	)
